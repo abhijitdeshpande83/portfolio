e3f4513d9d9205a640116fd5a7bdf517366cfdf1
Abhijit Deshpande <abhijitdeshpande83@gmail.com>
2024-09-17 15:17:56 -0400

    url.py updated

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ContactFormView  # Import your view
 
 urlpatterns = [
-    path('Contact Me/',ContactFormView.as_view(), name='contact_me'),
+    path('contact-me/',ContactFormView.as_view(), name='contact_me'),
 
     # Add other URL patterns specific to this app here
 ]
