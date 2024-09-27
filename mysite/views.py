21d552c4da9f5fbc3b4e3038988254ae6874aa79
Abhijit Deshpande <abhijitdeshpande83@gmail.com>
2024-09-26 21:32:01 -0500

    smtp connection setup for email

--- mysite/views.py
+++ mysite/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, FileResponse
 from .models import Certification, Tool, Experience
 from django.urls import reverse_lazy
+from django.core.mail import send_mail
 from django.views.generic.edit import FormView
 from .forms import ContactForm
 import os
@@ -37,6 +38,14 @@ class ContactFormView(FormView):
     def form_valid(self, form):
         if form.is_valid():
             form.save()
+                    # Send an email
+            send_mail(
+            'Contact Form Submission',
+            f'Thank you {form.cleaned_data["first_name"]} {form.cleaned_data["last_name"]}, your submission was successful!',
+            'abhijitdeshpande83@gmail.com',  # Replace with your email
+            [form.cleaned_data["email"]],
+            fail_silently=False,
+            )
             return super().form_valid(form)
         else:
             # Print or log form errors
