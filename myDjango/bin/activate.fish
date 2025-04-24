1ab17964b6efe09b7e88df1c5f7f9bcad30a4171
Ubuntu <ubuntu@ip-172-31-83-224.ec2.internal>
2025-04-24 05:06:27 +0000

    fixed env issue

--- myDjango/bin/activate.fish
+++ myDjango/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /Users/abhijitdeshpande/Documents/Portfolio/portfolio/myDjango
+set -gx VIRTUAL_ENV /home/ubuntu/portfolio/myDjango
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
