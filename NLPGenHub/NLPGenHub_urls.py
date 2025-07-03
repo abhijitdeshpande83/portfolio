924dd447030e4b3ab2b983bc9909df86ccb9710b
Abhijit Deshpande <abhijitdeshpande83@gmail.com>
2025-07-03 18:31:46 -0500

    Integrating SupportIQ project

--- NLPGenHub/NLPGenHub_urls.py
+++ NLPGenHub/NLPGenHub_urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .NLPGenHub_views import test
+from .NLPGenHub_views import test, intent_classify
 
 urlpatterns = [
     path('IntelliQA/', test, name='test'),
+    path('intent_classify/', intent_classify, name='intent-classify-api')
 ]
