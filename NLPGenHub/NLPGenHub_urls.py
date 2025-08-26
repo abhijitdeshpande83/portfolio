0df12e358e341da797ee5643969159c9746fa048
Abhijit Deshpande <abhijitdeshpande83@gmail.com>
2025-08-25 23:56:24 -0500

    chore(docker): add restart policy to all containers to auto-start

--- NLPGenHub/NLPGenHub_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .NLPGenHub_views import rag_intelliqa, intent_classify, rasa
-
-urlpatterns = [
-    path('IntelliQA/', rag_intelliqa, name='rag_intelliqa'),
-    path('intent_classify/', intent_classify, name='intent-classify-api'),
-    path('rasa_cinemora/', rasa, name='rasa-chatbot')
-]
